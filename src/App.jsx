efa0d4470afb9d9506e43f92d651e215b4289b86
Taricol <mozumder9122023@gmail.com>
2025-12-24 15:52:44 +0600

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,40 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
-      
-      
+      <Pet></Pet>
+      <Person></Person>
+      <Sports></Sports>
+      <Person></Person>
+      <Person></Person>
+      <Sports></Sports>
     </>
   )
 }
 
+function Person(){
+  const age=17;
+  const name='Jolil';
+  return(
+    <p>I am a person : {name} {age}</p>
+  )
+}
+
+function Pet(){
+  return(
+    <p>Cat</p>
+  )
+}
+
+function Sports(){
+  return(
+    <div>
+      <h3>Cricket</h3>
+      <ul>
+        <li>Kola</li>
+        <li>Komola</li>
+      </ul>
+      <p>Playing and loosing</p>
+    </div>
+  )
+}
 export default App
