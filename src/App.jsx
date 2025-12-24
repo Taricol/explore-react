39e4f8203add3947740fe28ce72ccb261c8b8c3f
Taricol <mozumder9122023@gmail.com>
2025-12-24 19:38:13 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,56 @@ function App() {
   return (
     <>
       
-      <h1>Vite + React</h1>
-      <Pet></Pet>
+      <h1>React Core Concepts</h1>
+      <Student name="Tahmid" dept="SWE"></Student>
+      <Student name="Sakib" dept="CSE"></Student>
       <Person></Person>
-      <Sports></Sports>
-      <Person></Person>
-      <Person></Person>
-      <Sports></Sports>
+      <Developer name="Mozumder" tech="JS"></Developer>
+      <Developer name="Rajon" tech="Phython"></Developer>
+      <Developer name="Shabana" tech="Java"></Developer>
+
+      
+
+      
     </>
   )
 }
 
+function Developer(props){
+  console.log(props);
+  return(
+    <div style={{
+      border:'2px solid green',
+      borderRadius:'20px'
+    }}>
+      <h3>Developer: {props.name} </h3>
+
+      <p>Technology: {props.tech}</p>
+
+    </div>
+  )
+}
+
+
 function Person(){
   const age=17;
   const name='Jolil';
+
+  const personStyle={
+    color:'red',
+    textAlign: 'right'
+  }
   return(
-    <p>I am a person : {name} {age}</p>
+    <p id='' title='tooltip' style={personStyle}>I am a person : {name} {age}</p>
+  )
+}
+
+function Student(props){
+  return(
+    <div className='student'>
+      <p>Name : {props.name} </p>
+      <p>Dept : {props.dept} </p>
+    </div>
   )
 }
 
